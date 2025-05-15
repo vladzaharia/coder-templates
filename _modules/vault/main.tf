da4f84d11a7f50f27b247d5bf6406963d3bda873
Vlad Zaharia <accounts@vlad.gg>
2025-05-15 04:33:37 +0000

    Default values with try

--- _modules/vault/main.tf
+++ _modules/vault/main.tf
@@ -35,7 +35,10 @@ data "coder_parameter" "vault_project" {
 }
 
 data "vault_generic_secret" "path" {
-  count = var.path != null || length(data.coder_parameter.vault_project) > 0 ? 1 : 0
-  path  = "dotenv/${var.path != null ? var.path : length(data.coder_parameter.vault_project) > 0 ? data.coder_parameter.vault_project[0].value : "_empty"}/dev"
+  path  = "dotenv/${var.path != null ? var.path : "_empty"}/dev"
+}
+
+data "vault_generic_secret" "dotenv" {
+  path  = "dotenv/${try(data.coder_parameter.vault_project[0].value, "") != "" ? data.coder_parameter.vault_project[0].value : "_empty"}/dev"
 }
 
